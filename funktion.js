59aa9161ca6c3c2a64244703ff88dec62e1914fa
DavvveN <65786975+DavvveN@users.noreply.github.com>
2020-05-25 21:14:03 -0400

    Add files via upload

---
+++ funktion.js
@@ -0,0 +1,12 @@
+var wincheck=[];
+
+function ruta1_O(){
+	var ruta1 =document.getElementById('ruta1');
+	element.classList.add("ruta1_O");
+	wincheck.push["O"];
+}
+function ruta1_X(){
+	var ruta1 =document.getElementById('ruta1');
+	element.classList.add("ruta1_X");
+	wincheck.push["X"];
+}
